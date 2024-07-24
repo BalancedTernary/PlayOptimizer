18656bc7f84a6db022da96512bbebcd7f4d1d3a5
BalancedTernary <70513163+BalancedTernary@users.noreply.github.com>
2024-07-24 10:42:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,21 +3,21 @@ import math
 from Play import Play
 
 maxTimes=100000
-BatchSize=3000
+BatchSize=1000
 
 matrixSize=8
-layers=400
+layers=200
 features=512
 
 Play_lr=2e-4
-Adam_lr=5e-5
+Adam_lr=2e-4
 weight_decay=0
 
 
 class Block(torch.nn.Module):
-    def __init__(self,in_features,out_features,gain,**kwargs):
+    def __init__(self,in_features,out_features,gain=1,**kwargs):
         super(Block, self).__init__()
-        self.Linear=torch.nn.Linear(in_features,out_features,bias=True)
+        self.Linear=torch.nn.Linear(in_features,out_features,bias=True,**kwargs)
         with torch.no_grad():
             torch.nn.init.orthogonal_(self.Linear.weight,gain)
             self.Linear.bias.fill_(0)
@@ -29,12 +29,16 @@ class Block(torch.nn.Module):
 class AccessNet(torch.nn.Module):
     def __init__(self,in_features,out_features,hidden_layers,hidden_features,**kwargs):
         super(AccessNet, self).__init__()
-        self.inLayer=torch.nn.Linear(in_features,hidden_features,bias=True)
+        self.inLayer=torch.nn.Linear(in_features,hidden_features,bias=True,**kwargs)
         self.Blocks=torch.nn.Sequential() 
         for t in range(hidden_layers):
-            self.Blocks.add_module('hiddenLayer{0}'.format(t),Block(hidden_features,hidden_features,1/math.sqrt(hidden_layers)))
-        self.outLayer=torch.nn.Linear(hidden_features,out_features,bias=True)
-        
+            self.Blocks.add_module('hiddenLayer{0}'.format(t),Block(hidden_features,hidden_features,**kwargs))
+        self.outLayer=torch.nn.Linear(hidden_features,out_features,bias=True,**kwargs)
+        with torch.no_grad():
+            torch.nn.init.orthogonal_(self.inLayer.weight,1)
+            self.inLayer.bias.fill_(0)
+            torch.nn.init.orthogonal_(self.outLayer.weight,1/math.sqrt(hidden_layers))
+            self.outLayer.bias.fill_(0)
     def forward(self,input):
         step=self.inLayer(input)
         outputAccess=torch.zeros_like(step)
@@ -44,21 +48,20 @@ class AccessNet(torch.nn.Module):
         output=self.outLayer(outputAccess)
         return output
 
-moduel1=AccessNet(matrixSize*matrixSize,matrixSize*matrixSize,layers,features)
-moduel2=AccessNet(matrixSize*matrixSize,matrixSize*matrixSize,layers,features)
-optimizer1 = torch.optim.Adam(moduel1.parameters(),Adam_lr,weight_decay=weight_decay)
-optimizer2 = Play(moduel2.parameters(),lr=Play_lr,weight_decay=weight_decay)
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-moduel1.to(device)
-moduel2.to(device)
+moduel1=AccessNet(matrixSize*matrixSize,matrixSize*matrixSize,layers,features,device=device)
+moduel2=AccessNet(matrixSize*matrixSize,matrixSize*matrixSize,layers,features,device=device)
+optimizer1 = torch.optim.RAdam(moduel1.parameters(),Adam_lr,weight_decay=weight_decay)
+optimizer2 = Play(moduel2.parameters(),lr=Play_lr,weight_decay=weight_decay)
+
 eye=torch.eye(matrixSize,device=device).unsqueeze(0)
+eye=eye/eye.std()
 
 import visdom
 wind = visdom.Visdom(env="Optimizer Test", use_incoming_socket=False)
     
-wind.line([[float('nan'),float('nan')]],[0],win = 'loss',opts = dict(title = 'log(loss)/log(batchs)',legend = ['Adam','Play']))
+wind.line([[float('nan'),float('nan')]],[0],win = 'loss',opts = dict(title = 'log(loss)/log(batchs)',legend = ['RAdam','Play']))
 
 print(moduel1)
     
@@ -68,7 +71,9 @@ for time in range(maxTimes):
     output=moduel1(input)
     input=input.view(BatchSize,matrixSize,matrixSize)
     output=output.view(BatchSize,matrixSize,matrixSize)
-    loss1=(input@output-eye).abs().mean()
+    e=input@output
+    e=e/e.std()
+    loss1=(e-eye).abs().mean()
     loss1.backward()
     optimizer1.step()
     
@@ -77,9 +82,11 @@ for time in range(maxTimes):
     output=moduel2(input)
     input=input.view(BatchSize,matrixSize,matrixSize)
     output=output.view(BatchSize,matrixSize,matrixSize)
-    loss2=(input@output-eye).abs().mean()
+    e=input@output
+    e=e/e.std()
+    loss2=(e-eye).abs().mean()
     loss2.backward()
     optimizer2.step(loss=loss2)
     
     wind.line([[float(loss1.log()),float(loss2.log())]],[math.log(time+1)],win = 'loss',update = 'append')
-    #wind.line([[float(loss1),float(loss2)]],[time],win = 'loss',update = 'append')
+    
